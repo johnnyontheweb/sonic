59078c3a5ed6a91f0198470ae8f93aecb386b2f1
Giovanni <giovannirinaldin@hotmail.com>
2023-09-22 20:03:26 +0200

    allow repeated names in variables

--- Adletec.Sonic/Util/EngineUtil.cs
+++ Adletec.Sonic/Util/EngineUtil.cs
@@ -9,9 +9,38 @@ namespace Adletec.Sonic.Util
     /// </summary>
     internal static class EngineUtil
     {
-        internal static IDictionary<string, double> ConvertVariableNamesToLowerCase(IDictionary<string, double> variables)
+        static internal IDictionary<string, double> ConvertVariableNamesToLowerCase(IDictionary<string, double> variables)
         {
-            return new Dictionary<string, double>(variables, StringComparer.OrdinalIgnoreCase);
+            Dictionary<string, double> temp = new Dictionary<string, double>();
+            foreach (KeyValuePair<string, double> keyValuePair in variables)
+            {
+                string keyL = keyValuePair.Key.ToLowerFast();
+                if (!temp.ContainsKey(keyL)) temp.Add(keyL, keyValuePair.Value); else temp[keyL] = keyValuePair.Value;
+            }
+
+            return temp;
+        }
+
+        // This is a fast ToLower for strings that are in ASCII
+        static internal string ToLowerFast(this string text)
+        {
+            StringBuilder buffer = new StringBuilder(text.Length);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                if (c >= 'A' && c <= 'Z')
+                {
+                    buffer.Append((char)(c + 32));
+                }
+                else
+                {
+                    buffer.Append(c);
+                }
+            }
+
+            return buffer.ToString();
         }
     }
 }
